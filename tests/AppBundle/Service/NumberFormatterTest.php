22e2e69a5d6276e10a1e4340096a9fe0f9645961
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-11-14 12:55:24 +0200

    Added phpunit to composer

--- tests/AppBundle/Service/NumberFormatterTest.php
+++ tests/AppBundle/Service/NumberFormatterTest.php
@@ -6,7 +6,49 @@
  * Time: 12:29 PM
  */
 
-class NumberFormatterTest
+namespace Tests\AppBundle\Service;
+
+use AppBundle\Service\NumberFormatter;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class NumberFormatterTest extends TestCase
 {
+
+    /**
+     * @dataProvider formatProvider
+     */
+    public function testFormat(float $num, string $result) : void
+    {
+
+        $numFormatter = new NumberFormatter();
+        $subject = $numFormatter->format($num);
+
+        $this->assertEquals($result, $subject);
+
+    }
+
+    public function formatProvider() : array
+    {
+
+        return [
+            '1' => [2835779, '2.84M'],
+            '2' => [9995000, '1.00M'],
+            '3' => [535216, '535K'],
+            '4' => [99950, '100K'],
+            '5' => [27533.78, '27 534'],
+            '6' => [533.1, '533.10'],
+            '7' => [66.6666, '66.67'],
+            '8' => [12.00, '12'],
+            '-1' => [2835779, '-2.84M'],
+            '-2' => [9995000, '-1.00M'],
+            '-3' => [535216, '-535K'],
+            '-4' => [99950, '-100K'],
+            '-5' => [27533.78, '-27 534'],
+            '-6' => [533.1, '-533.10'],
+            '-7' => [66.6666, '-66.67'],
+            '-8' => [12.00, '-12'],
+        ];
+
+    }
 
 }

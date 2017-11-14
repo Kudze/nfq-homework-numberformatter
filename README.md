e339dcdf0fe8c69f8da11665df22e15fec007156
Karolis Kraujelis <kudziokaschannel@gmail.com>
2017-11-14 12:28:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+homework6
+=========
+
+A Symfony project created on November 14, 2017, 12:15 pm.
---
+++ src/AppBundle/Service/NumberFormatter.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kkraujelis
+ * Date: 11/14/17
+ * Time: 12:25 PM
+ */
+
+namespace AppBundle\Service;
+
+class NumberFormatter
+{
+
+    public function format(float $num) : string
+    {
+
+        return "This hasnt been implemented yet";
+
+    }
+
+}
